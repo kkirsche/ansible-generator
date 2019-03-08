156d44b347f024ecb2d517e7720afded590a7d43
Kevin Kirsche <kev.kirsche@gmail.com>
2019-03-08 14:31:22 -0500

    Ensure that printed directories are joined rather than u strings

--- ansible_generator/directories.py
+++ ansible_generator/directories.py
@@ -35,11 +35,11 @@ def create_directory_layout(
 
     logger.debug(
         'msg="{n} required directories" directories="{p}"'.format(
-            n=len(required_paths), p=required_paths
+            n=len(required_paths), p=', '.join(required_paths)
         )
     )
     if projects:
-        logger.debug('msg="projects was defined" projects="{p}"'.format(p=projects))
+        logger.debug('msg="projects was defined" projects="{p}"'.format(p=', '.join(projects)))
 
         final_paths = []
         for project in projects:
@@ -50,7 +50,7 @@ def create_directory_layout(
         required_paths = final_paths
         logger.debug(
             'msg="{n} project required directories" directories="{p}"'.format(
-                n=len(required_paths), p=required_paths
+                n=len(required_paths), p=', '.join(required_paths)
             )
         )
 
