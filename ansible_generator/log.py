9945fe46041083c8f5cef8b22d35adeb5adb4991
Kevin Kirsche <kev.kirsche@gmail.com>
2019-03-09 09:15:01 -0500

    Upgrade from raven to sentry-sdk

--- ansible_generator/log.py
+++ ansible_generator/log.py
@@ -1,32 +1,27 @@
 # -*- coding: utf-8 -*-
 """Create a logger instance """
 # third party packages
-from raven import Client, fetch_package_version
-from raven.conf import setup_logging
-from raven.handlers.logging import SentryHandler
+from sentry_sdk import init
+from sentry_sdk.integrations.logging import LoggingIntegration
 
 # python stdlib
+from pkg_resources import get_distribution
 import logging
 
 
-def get_sentry_handler():
-    client = Client(
-        dsn=(
-            "https://036bd28e074a4a4a99712dc05c9f768e:d4bbdc0c3ca7495aa2bb4a"
-            "cf1ba25d88@sentry.io/202195"
-        ),
-        release=fetch_package_version("ansible-generator"),
+def configure_sentry():
+    sentry_logging = LoggingIntegration(level=logging.INFO, event_level=logging.ERROR)
+    init(
+        dsn="https://036bd28e074a4a4a99712dc05c9f768e@sentry.io/202195",
+        integrations=[sentry_logging],
+        release=get_distribution("ansible-generator").version,
     )
-    handler = SentryHandler(client)
-    handler.setLevel(logging.ERROR)
-    return handler
 
 
 def setup_logger(name, log_level=logging.INFO):
     log_format = "%(message)s"
     logging.basicConfig(format=log_format)
-    handler = get_sentry_handler()
-    setup_logging(handler)
+    configure_sentry()
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     return logger
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.3',
+    version='2.1.0',
     description='Generate ansible directory structures',
     long_description=long_description,
 
@@ -80,7 +80,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['raven', 'ansible'],
+    install_requires=['setuptools', 'sentry-sdk', 'ansible'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
